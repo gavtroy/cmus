67ff2d991b86be316816efeab1821b815841da37
Gavin Troy <gavin@cantlogout.com>
2023-07-21 21:43:30 +0100

    Add %{albumtracks} format string
    Total (counted) number of tracks in album. Also works as %N.
    
    Closes #850.

--- tree.c
+++ tree.c
@@ -555,6 +555,7 @@ static struct album *album_new(struct artist *artist, const char *name,
 	album->min_date = date;
 	rb_root_init(&album->track_root);
 	album->artist = artist;
+	album->num_tracks = 0;
 
 	return album;
 }
@@ -870,6 +871,9 @@ static void album_add_track(struct album *album, struct tree_track *track)
 
 	rb_link_node(&track->tree_node, parent, new);
 	rb_insert_color(&track->tree_node, &album->track_root);
+
+	album->num_tracks++;
+	tree_track_info(track)->lib_album = album;
 }
 
 const char *tree_artist_name(const struct track_info* ti)
@@ -1180,6 +1184,9 @@ static void remove_track(struct tree_track *track)
 		window_row_vanishes(lib_track_win, (struct iter *)&iter);
 	}
 	rb_erase(&track->tree_node, &track->album->track_root);
+
+	track->album->num_tracks--;
+	tree_track_info(track)->lib_album = NULL;
 }
 
 void tree_remove(struct tree_track *track,
