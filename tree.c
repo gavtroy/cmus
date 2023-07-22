7697e0ab1a498793febb4819ee10cb3d14cea0d2
Gavin Troy <gavin@cantlogout.com>
2023-07-22 02:16:12 +0100

    Experiment: Add option for flat library view

--- tree.c
+++ tree.c
@@ -216,7 +216,7 @@ static int tree_get_prev(struct iter *iter)
 			return 0;
 		}
 		artist = to_artist(rb_last(root));
-		if (artist->expanded) {
+		if (artist->expanded || flat_library_view) {
 			album = to_album(rb_last(&artist->album_root));
 		} else {
 			album = NULL;
@@ -225,11 +225,13 @@ static int tree_get_prev(struct iter *iter)
 		iter->data2 = album;
 		return 1;
 	}
-	if (artist->expanded && album) {
+	if ((artist->expanded || flat_library_view) && album) {
 		/* prev album */
 		if (rb_prev(&album->tree_node) == NULL) {
-			iter->data2 = NULL;
-			return 1;
+			if (!flat_library_view) {
+				iter->data2 = NULL;
+				return 1;
+			}
 		} else {
 			iter->data2 = to_album(rb_prev(&album->tree_node));
 			return 1;
@@ -245,7 +247,7 @@ static int tree_get_prev(struct iter *iter)
 	artist = to_artist(rb_prev(&artist->tree_node));
 	iter->data1 = artist;
 	iter->data2 = NULL;
-	if (artist->expanded) {
+	if (artist->expanded || flat_library_view) {
 		/* last album */
 		iter->data2 = to_album(rb_last(&artist->album_root));
 	}
@@ -266,11 +268,13 @@ static int tree_get_next(struct iter *iter)
 			/* empty, iter points to the head already */
 			return 0;
 		}
-		iter->data1 = to_artist(rb_first(root));
-		iter->data2 = NULL;
-		return 1;
+		iter->data1 = artist = to_artist(rb_first(root));
+		if (!flat_library_view) {
+			iter->data2 = NULL;
+			return 1;
+		}
 	}
-	if (artist->expanded) {
+	if (artist->expanded || flat_library_view) {
 		/* next album */
 		if (album == NULL) {
 			/* first album */
@@ -289,8 +293,12 @@ static int tree_get_next(struct iter *iter)
 		iter->data2 = NULL;
 		return 0;
 	}
-	iter->data1 = to_artist(rb_next(&artist->tree_node));
-	iter->data2 = NULL;
+	iter->data1 = artist = to_artist(rb_next(&artist->tree_node));
+	if (!flat_library_view) {
+		iter->data2 = NULL;
+		return 1;
+	}
+	iter->data2 = to_album(rb_first(&artist->album_root));
 	return 1;
 }
 /* }}} */
@@ -732,7 +740,9 @@ static int tree_search_matches(void *data, struct iter *iter, const char *text)
 	if (!track_info_matches(tree_track_info(track), text, flags))
 		return 0;
 
-	if (auto_expand_albums_search) {
+	if (flat_library_view) {
+		album_to_iter(track->album, &tmpiter);
+	} else if (auto_expand_albums_search) {
 		/* collapse old search result */
 		if (collapse_artist) {
 			struct artist *artist = do_find_artist(collapse_artist, &lib_artist_root, NULL, NULL);
@@ -896,7 +906,7 @@ const char *tree_album_name(const struct track_info* ti)
 
 static void remove_album(struct album *album)
 {
-	if (album->artist->expanded) {
+	if (album->artist->expanded || flat_library_view) {
 		struct iter iter;
 
 		album_to_iter(album, &iter);
@@ -910,7 +920,8 @@ static void remove_artist(struct artist *artist)
 	struct iter iter;
 
 	artist_to_iter(artist, &iter);
-	window_row_vanishes(lib_tree_win, &iter);
+	if (!flat_library_view)
+		window_row_vanishes(lib_tree_win, &iter);
 	rb_erase(&artist->tree_node, &lib_artist_root);
 }
 
@@ -1272,7 +1283,7 @@ void tree_sel_track(struct tree_track *t, int auto_expand_albums)
 
 		if (auto_expand_albums)
 			t->album->artist->expanded = 1;
-		if (t->album->artist->expanded)
+		if (t->album->artist->expanded || flat_library_view)
 			album_to_iter(t->album, &iter);
 		else
 			artist_to_iter(t->album->artist, &iter);
--- ui_curses.c
+++ ui_curses.c
@@ -684,7 +684,12 @@ static void print_tree(struct window *win, int row, struct iter *iter)
 	}
 
 	gbuf_add_ch(&print_buffer, ' ');
-	if (album) {
+	if (flat_library_view) {
+		if (album == NULL)
+			return;
+		fill_track_fopts_album(album);
+		format_print(&print_buffer, tree_win_w - 1, tree_win_flat_format, track_fopts);
+	} else if (album) {
 		fill_track_fopts_album(album);
 		format_print(&print_buffer, tree_win_w - 1, tree_win_format, track_fopts);
 	} else {
